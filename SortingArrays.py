adeb9c847606efb7442603db851fd92a2d41a2df
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-06-05 09:41:43 +0300

    Sorting Numpy arrays

---
+++ SortingArrays.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+#Sorting a 1D Numpy array
+n=np.array([1,4,2,9,0,5,3,5,33,2,5,32,55,33])
+print(np.sort(n))
+
+p=np.array(['jj','python','android','kotlin','javascript','motoko'])
+print(np.sort(p))
+
+#Sorting a 2D Numpy array
+q=np.array([[5,3,2,9,1],[7,3,9,0,6]])
+print(np.sort(q))
